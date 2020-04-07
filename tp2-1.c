3e640ab127dd9d94eef76a57e1381500b7255002
Leo <programa0.1@outlook.com>
2020-04-07 16:31:40 -0300

    rama numero 1

--- tp2-1.c
+++ tp2-1.c
@@ -5,11 +5,14 @@
 
 int main (){
 	int f,c;	
-	double mt[N][M];
+	double mt[N][M], *p_mt;
+    p_mt = &mt[0][0];
+
 		
     for(f = 0;f<N; f++) {
         for(c = 0;c<N; c++){
-            printf("|%lf|", mt[f][c]);
+            printf("|%lf|", p_mt);
+            p_mt++;
             }
         printf("\n");
         }
