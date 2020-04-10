7375c143fc546d5ad2e9b4526ea7fb4d0dbfa2c5
Leo <programa0.1@outlook.com>
2020-04-10 18:53:32 -0300

    segundo commit

--- tp2-2.c
+++ tp2-2.c
@@ -4,18 +4,51 @@
 
 int main(){
     int fil =15, col,i,j;
+    int contPar=0, contImp=0, contImpd=0,contPard=0;
+    int *vec;
     srand(time(NULL));
     col = rand() %16;
 
     int mt[fil][col];
 
-    for ( i = 0; i < col; i++){
-        for(j=0;j<fil;j++){
+	vec = (int*)malloc(15*sizeof(int));
+    if (vec == NULL){
+        printf("espacio insuficiente");
+    }
+
+    for ( i = 0; i < fil; i++){
+        for(j=0;j<col;j++){
             mt[i][j] =100+rand() % (1000 - 100);
+            if ( mt[i][j]%2==0) {
+                contPar++;
+            }
+            else{
+                contImp++;
+            }
+            
+            
             printf("|%d|", mt[i][j]);
         }
         printf("\n");
     }
+
+    printf("\n\n numeros pares= %d\n numeros impares= %d", contPar,contImp);
     
+    
+    
+    printf("\n\n");
+    for ( i = 0; i < 15; i++){
+            vec[i] =100+rand() % (1000 - 100);
+            if ( vec[i]%2==0) {
+                contPard++;
+            }
+            else{
+                contImpd++;
+            }
+            printf("|%d|", vec[i]);
+    }
+	printf("\n\n numeros pares= %d\n numeros impares= %d\n", contPard,contImpd);
+
+    system("pause");
     return 0;
 }
